e62ddf48a0b72f7cde179153bb3d5b1a80f89442
abwhitley <a.blakewhitley@gmail.com>
2016-08-11 16:25:43 -0400

    Initial Commit

---
+++ madLib/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  madLib
+//
+//  Created by Austins Work on 8/11/16.
+//  Copyright © 2016 AustinsIronYard. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
